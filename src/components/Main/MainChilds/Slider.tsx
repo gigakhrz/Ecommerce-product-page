757602bb90b70a0d5e95e8e0e4db83b4f850ed0d
gigakhrz <kharazishvilig13@gmail.com>
2023-06-02 14:38:36 +0400

    change x icon size

--- src/components/Main/MainChilds/Slider.tsx
+++ src/components/Main/MainChilds/Slider.tsx
@@ -46,16 +46,19 @@ const Slider = ({ openImg, setOpenImg }: SliderProps): JSX.Element => {
       <div className="desktopContainer">
         <svg
           className="close"
-          width="14"
-          height="15"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-            fill="#69707D"
-            fill-rule="evenodd"
+            d="M2,2 L18,18 M18,2 L2,18"
+            fill="none"
+            stroke="white"
+            strokeWidth="4"
           />
         </svg>
+
         <div className="productCont">
           <img className="product" src={photo} alt="" />
 

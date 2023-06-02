230811de00a9f6c2ad898286f030ad1b8b93e760
gigakhrz <kharazishvilig13@gmail.com>
2023-06-02 14:29:35 +0400

    replace img by svg close icon

--- src/components/Main/MainChilds/Slider.tsx
+++ src/components/Main/MainChilds/Slider.tsx
@@ -13,8 +13,6 @@ import mini2 from "../../../assets/image-product-2-thumbnail.jpg";
 import mini3 from "../../../assets/image-product-3-thumbnail.jpg";
 import mini4 from "../../../assets/image-product-3-thumbnail.jpg";
 
-import xIcon from "../../../assets/icon-close.svg";
-
 interface SliderProps {
   openImg: boolean;
   setOpenImg: (openImg: boolean) => void;
@@ -46,7 +44,18 @@ const Slider = ({ openImg, setOpenImg }: SliderProps): JSX.Element => {
     <SlyderContainer openImg={openImg}>
       {/* this productCont only appear on mobile,tablet, and on desktop when user clicked product photo to enlarge it */}
       <div className="desktopContainer">
-        <img className="close" src={xIcon} alt=" close icon" />
+        <svg
+          className="close"
+          width="14"
+          height="15"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+            fill="#69707D"
+            fill-rule="evenodd"
+          />
+        </svg>
         <div className="productCont">
           <img className="product" src={photo} alt="" />
 
